0ee4a0560aef48e22075f9f0284886d14e23b2a5
CoderZIE <102377644+CoderZIE@users.noreply.github.com>
2022-11-07 19:17:14 +0530

    Delete navbar+footer.js

--- navbar+footer.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".zwrap").style.display = "none"
-function preloaderFunction() {
-
-  document.querySelector(".Rpreload").style.display = "none"
-  document.querySelector(".zwrap").style.display = "initial"
-}
